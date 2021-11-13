2f5420948c6cab959d334c446d26b5207c764430
rluj <94225655+rluj@users.noreply.github.com>
2021-11-13 15:09:28 +0100

    Exercise 01-02 and 01-03

---
+++ my-first-application/src/app/course-list-item/course-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list-item',
+  templateUrl: './course-list-item.component.html',
+  styleUrls: ['./course-list-item.component.scss']
+})
+export class CourseListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ my-first-application/src/app/course/course.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseListItemComponent } from '../course-list-item/course-list-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    CourseListItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CourseModule { }
